73e819c6ed27fd05badaaa57109248021cb138d7
Jiro Matsumoto <114434414+GoldenDev91@users.noreply.github.com>
2022-10-23 01:28:35 -0400

    update: handled BAD_REQUEST error

--- index.mjs
+++ index.mjs
@@ -5,61 +5,71 @@ import { default as axios } from "axios";
 import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
 import { delay } from "./utils.js";
-import { Console } from "node:console";
+import { Console, timeLog } from "node:console";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const file = join(__dirname, "db.json");
 const adapter = new JSONFile(file);
 const db = new Low(adapter);
-await db.read();
 db.data ||= {
   coingecko: {
     list: [],
     coins: [],
   },
 };
+await db.write();
+await db.read();
 
 async function getCoingecko() {
-  try {
-    const list_response = await axios.get(
-      "https://api.coingecko.com/api/v3/coins/list",
-      {
-        params: {
-          include_platform: false,
-        },
-      }
-    );
-    var coin_list = list_response.data;
-    db.data.coingecko.list = coin_list;
-    await db.write();
-
-    let coin_details = [];
-    for (let coin_idx in db.data.coingecko.list) {
-      let coin = db.data.coingecko.list[coin_idx];
-      const detail_response = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/" +
-          coin.id +
-          "?localization=false&tickers=false&market_data=false&community_data=true&developer_data=false&sparkline=false"
-      );
-      // {
-      // localization: "false",
-      // tickers: "false",
-      // market_data: "false",
-      // community_data: "true",
-      // developer_data: "false",
-      // sparkline: "false",
-      // }
-      console.log(coin_idx + " : " + detail_response.data.name);
-      db.data.coingecko.coins.push(detail_response.data);
+  await axios
+    .get("https://api.coingecko.com/api/v3/coins/list", {
+      params: {
+        include_platform: false,
+      },
+    })
+    .then(async (res) => {
+      db.data.coingecko.list = res.data;
       await db.write();
-      await delay(2000);
-    }
-    db.data.coingecko.coin = coin_data;
-  } catch (error) {
-    console.error(error);
+      console.log(res.data.length + " coins saved.");
+    })
+    .catch(async (err) => console.error(err.code));
+  for (
+    let coin_idx = db.data.coingecko.coins.length;
+    coin_idx < db.data.coingecko.list.length;
+    coin_idx++
+  ) {
+    await axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/" +
+          db.data.coingecko.list[coin_idx].id +
+          "?localization=false&tickers=false&market_data=false&community_data=true&developer_data=false&sparkline=false"
+      )
+      .then(async (res) => {
+        console.log(coin_idx + " : " + res.data.name);
+        db.data.coingecko.coins.push(res.data);
+        await db.write();
+        await delay(2000);
+      })
+      .catch(async (err) => {
+        console.error(err.code);
+        let time_left = 60;
+        console.log("waiting for 60 seconds");
+        while (time_left-- > 60) {
+          console.log(`remaining {} seconds ...`);
+          await delay(1000);
+        }
+        coin_idx--;
+      });
+    // {
+    // localization: "false",
+    // tickers: "false",
+    // market_data: "false",
+    // community_data: "true",
+    // developer_data: "false",
+    // sparkline: "false",
+    // }
   }
 }
-
 getCoingecko();
 await db.write();
