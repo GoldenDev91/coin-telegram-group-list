79324c1486462962f176db3062ed7399af29a13e
Jiro Matsumoto <114434414+GoldenDev91@users.noreply.github.com>
2022-10-23 01:33:19 -0400

    update: fixed typo

--- index.mjs
+++ index.mjs
@@ -56,7 +56,7 @@ async function getCoingecko() {
         let time_left = 60;
         console.log("waiting for 60 seconds");
         while (time_left-- > 60) {
-          console.log(`remaining {} seconds ...`, time_left);
+          console.log(`remaining {time_left} seconds ...`);
           await delay(1000);
         }
         coin_idx--;
